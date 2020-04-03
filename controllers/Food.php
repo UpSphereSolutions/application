02f9b9060da685a2d08d1c3989e1265f5a1cffa0
Rb Andoy <andoyandoy5@gmail.com>
2020-04-04 01:57:12 +0800

    'add'

--- controllers/Food.php
+++ controllers/Food.php
@@ -6,5 +6,15 @@ class Food extends CI_Controller {
 	{
         echo json_encode(array(array("name"=>"rb", "img"=>"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png", "prince"=> "100.00"),
                    array("name"=>"rb", "img"=>"https://cdn.vox-cdn.com/thumbor/2Gx0MqNg5DKbzE9sD2uTSGKFNVM=/0x0:1000x604/1200x800/filters:focal(420x222:580x382)/cdn.vox-cdn.com/uploads/chorus_image/image/58245867/GooglePay_Lockup.max_1000x1000.0.png", "prince"=> "100.00")));
+    }
+    public function login()
+	{ 
+        $username = $this->input->post('username');
+        $password = $this->input->post('password'); 
+        if ($username == 'admin' && $password == 'admin') {
+            echo json_encode(array('Message'=> 'Welcome User'));
+        } else {
+            echo json_encode(array('Message'=> 'Invalid Username/Password'));
+        }
 	}
 }
