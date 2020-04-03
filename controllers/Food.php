9291ff30dc8680baa114f86f1dd59153ae13b653
Rb Andoy <andoyandoy5@gmail.com>
2020-04-03 20:32:39 +0800

    change image url

--- controllers/Food.php
+++ controllers/Food.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Food extends CI_Controller {
 	public function index()
 	{
-        echo json_encode(array(array("name"=>"rb", "img"=>"hjshjdhka.png", "prince"=> "100.00"),
-                   array("name"=>"rb", "img"=>"hjshjdhka.png", "prince"=> "100.00")));
+        echo json_encode(array(array("name"=>"rb", "img"=>"https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png", "prince"=> "100.00"),
+                   array("name"=>"rb", "img"=>"https://cdn.vox-cdn.com/thumbor/2Gx0MqNg5DKbzE9sD2uTSGKFNVM=/0x0:1000x604/1200x800/filters:focal(420x222:580x382)/cdn.vox-cdn.com/uploads/chorus_image/image/58245867/GooglePay_Lockup.max_1000x1000.0.png", "prince"=> "100.00")));
 	}
 }
