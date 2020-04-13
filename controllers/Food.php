6b7c2c89a7fc9f235c611e03f8462ef0744eea62
Rb Andoy <andoyandoy5@gmail.com>
2020-04-14 00:34:04 +0800

    'add'

--- controllers/Food.php
+++ controllers/Food.php
@@ -5,6 +5,7 @@ class Food extends CI_Controller {
     function __construct() {
         parent::__construct();
         $this->load->model('Food_model');
+        $this->load->library('email');
     }
 
 	public function index()
@@ -66,5 +67,58 @@ class Food extends CI_Controller {
             echo json_encode('USER NOT FOUND');
         }
         
+    }
+
+
+    public function sendEmail() {
+        //SMTP & mail configuration
+        $this->load->library('encryption');
+        $config = array(
+            'protocol'  => 'smtp',
+            'smtp_host' => 'ssl://box5742.bluehost.com',
+            'smtp_port' => 465,
+            'smtp_user' => 'upsphere@upspheresolutions.com',
+            'smtp_pass' => 'TheSphere@2020',
+            'mailtype'  => 'html',
+            'charset'   => 'utf-8'
+        );
+        $this->email->initialize($config);
+        $this->email->set_mailtype("html");
+        $this->email->set_newline("\r\n");
+        //Email content
+        $htmlContent = '<h1 style="color: #5e9ca0;">UPSPHERE SOLUTIONS</h1>';
+        $htmlContent .= '<p><span style="color: #0000ff;"><span style="color: #808080;">Social Media:</span></span></p>';
+        $htmlContent .= '<p><span style="color: #0000ff;"><span style="color: #808080;">Social Media:</span></span></p>';
+        $htmlContent .= '<ol style="list-style: none; font-size: 14px; line-height: 32px; font-weight: bold;">';
+        $htmlContent .= '<li style="clear: both;"><span style="color: #0000ff;"><a href="https://www.facebook.com/UpSphere-Solutions-';
+        $htmlContent .= '102288094758962/" target="_blank" rel="noopener">Upsphere Solutions</a><img style="float: left;"';
+        $htmlContent .= 'src="https://encrypted-tbn0.gstatic.com/images?';
+        $htmlContent .= 'q=tbn%3AANd9GcSAqrpvltqgLoZQRSNgBtsT2L5gWpMpUzU9QLB-KLSXxtSZe768&amp;usqp=CAU"';
+        $htmlContent .= 'alt="interactive connection" width="43" height="43" /></span></li>';
+        $htmlContent .= '<p><span style="color: #0000ff;"><span style="color: #808080;">Email: <a href="">upsphere@upspheresolutions.com</a></span></span></p>';
+        $htmlContent .= '</ol>';   
+        $this->email->to('andoyandoy5@gmail.com');
+        $this->email->from('upsphere@upspheresolutions.com','UPSPHERE SOLUTIONS');
+        $this->email->subject('NO REPLY!!');
+        $this->email->message($htmlContent);
+        //Send email
+        $res = $this->email->send();
+        echo json_encode($res);
+    }
+
+    public function uploadImage() {
+        $this->load->library('ftp');
+        $config['hostname'] = 'ftp://ftp.ghd.qqt.mybluehost.me';
+        $config['username'] = 'upsphere@ghd.qqt.mybluehost.me';
+        $config['password'] = 'TheSphere@2020';
+        $config['port'] = '21';
+        $config['debug']        = TRUE;
+        $this->ftp->connect($config);
+        $fname = 'C:/Users/retchel/Downloads/file.png';
+        if (file_exists($fname)) { 
+            $res = $this->ftp->upload($fname, '/public_html/upsphere/file.png', 'ascii', 0775);
+        } else { echo 'File not found!'; }
+      
+        $this->ftp->close(); 
     }
 }
