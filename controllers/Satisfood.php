eb6550db4a7761fcef88c1ec885ba1948eb79dd3
Rb Andoy <andoyandoy5@gmail.com>
2020-05-15 06:40:05 +0800

    asd

--- controllers/Satisfood.php
+++ controllers/Satisfood.php
@@ -1,6 +1,13 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed'); 
- 
+header('Set-Cookie: cross-site-cookie=name; SameSite=None; Secure');
+header("Access-Control-Allow-Headers: Authorization");
+header("Access-Control-Allow-Origin: *"); 
+header('HTTP/1.0 200 OK');
+header('HTTP/1.1 200 OK'); 
+header('Cache-Control: no-store, no-cache, must-revalidate');
+header('Cache-Control: post-check=0, pre-check=0');
+header('Pragma: no-cache');
 class Satisfood extends CI_Controller {
 
     function __construct() {
@@ -8,14 +15,7 @@ class Satisfood extends CI_Controller {
         $this->load->model('Food_model');
         $this->load->library('email');
         $this->load->library('session');
-        header('Set-Cookie: cross-site-cookie=name; SameSite=None; Secure');
-        header("Access-Control-Allow-Headers: Authorization");
-        header("Access-Control-Allow-Origin: *"); 
-        header('HTTP/1.0 200 OK');
-        header('HTTP/1.1 200 OK'); 
-        header('Cache-Control: no-store, no-cache, must-revalidate');
-        header('Cache-Control: post-check=0, pre-check=0');
-        header('Pragma: no-cache');
+        
         $this->load->helper('url'); 
         // print_r($this->session->userdata('user'));exit();
         $this->user = $this->session->userdata('user');
