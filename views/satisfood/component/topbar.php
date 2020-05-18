181f47606ad511ba687cd635246c4410022bb03c
Rb Andoy <thespheresolutions@gmail.com>
2020-05-18 10:23:15 +0800

    update

--- views/satisfood/component/topbar.php
+++ views/satisfood/component/topbar.php
@@ -48,11 +48,10 @@
 
   <!-- Nav Item - Alerts -->
   <li class="nav-item dropdown no-arrow mx-1">
-    <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      <i class="fas fa-bell fa-fw"></i>
-      <!-- Counter - Alerts -->
+    <!-- <a class="nav-link dropdown-toggle" href="#" id="alertsDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <i class="fas fa-bell fa-fw"></i> 
       <span class="badge badge-danger badge-counter">3+</span>
-    </a>
+    </a> -->
     <!-- Dropdown - Alerts -->
     <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="alertsDropdown">
       <h6 class="dropdown-header">
@@ -97,11 +96,10 @@
 
   <!-- Nav Item - Messages -->
   <li class="nav-item dropdown no-arrow mx-1">
-    <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-      <i class="fas fa-envelope fa-fw"></i>
-      <!-- Counter - Messages -->
+    <!-- <a class="nav-link dropdown-toggle" href="#" id="messagesDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <i class="fas fa-envelope fa-fw"></i> 
       <span class="badge badge-danger badge-counter">7</span>
-    </a>
+    </a> -->
     <!-- Dropdown - Messages -->
     <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="messagesDropdown">
       <h6 class="dropdown-header">
