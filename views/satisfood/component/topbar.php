dd872f3201183e95812b619c3fbbfa02a8b48eab
Rb Andoy <thespheresolutions@gmail.com>
2020-05-18 10:22:17 +0800

    update

--- views/satisfood/component/topbar.php
+++ views/satisfood/component/topbar.php
@@ -168,7 +168,7 @@
         <i class="fas fa-email fa-sm fa-fw mr-2 text-gray-400"></i>
         <span class="mr-2 d-none d-lg-inline text-gray-600 small"><?php echo ($this->user['email']); ?></span>
       </a>
-      <a class="dropdown-item" onclick="navigate('profile')">
+      <a class="dropdown-item pointer" color="black" onclick="navigate('profile')">
         <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
         Profile
       </a>
