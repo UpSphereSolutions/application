d8995fd08b61d62479b8c251b10f847aa07cf975
Rb Andoy <thespheresolutions@gmail.com>
2020-06-01 07:57:05 +0800

    update

--- models/Food_model.php
+++ models/Food_model.php
@@ -43,7 +43,9 @@ public function updateProfile($query){
 }
 
 public function getList() {
-        $result = $this->db->query("SELECT * FROM user_details INNER JOIN `user_address` ON `user_address`.`id` = `user_details`.`address`");
+        $result = $this->db->query("SELECT `user_details`.`id`,`user_details`.`fname`,`user_details`.`mname`,`user_details`.`lname`,`user_details`.`bdate`,`user_address`.`street`,`user_address`.`city`,`user_address`.`country`,`user_address`.`pcode`,`user_details`.`contactno`,`user_details`.`email`,`user_details`.`gender`, `user_details`.`status`, `user_category`.`description` FROM user_details 
+        INNER JOIN `user_address` ON `user_address`.`id` = `user_details`.`address` INNER JOIN `user_category` ON `user_category`.`id` = `user_details`.`type`
+        ");
         $result =  $result->result_array(); 
         return $result;
 }
@@ -60,27 +62,26 @@ public function sendEmail($username, $password) {
         mysqli_next_result( $this->db->conn_id );
         // print_r($result[0]['email']);exit();
         if (count($result) > 0) {
+                $params = array('username' => $username, 'password' => $password, 'ip' => $this->input->ip_address());
                 $this->load->library('encryption');
                 $config = array(
-                    'protocol'  => 'smtp',
-                    'smtp_host' => 'ssl://mail.upspheresolutions.com',
-                    'smtp_port' => 465,
-                    'smtp_user' => 'no-reply@upspheresolutions.com',
-                    'smtp_pass' => 'TheSphere@2020',
-                    'mailtype'  => 'html',
-                    'charset'   => 'utf-8'
+                'protocol'  => 'smtp',
+                'smtp_host' => 'ssl://mail.upspheresolutions.com',
+                'smtp_port' => 465,
+                'smtp_user' => 'satisfood@upspheresolutions.com',
+                'smtp_pass' => 'satisfood',
+                'mailtype'  => 'html',
+                'charset'   => 'utf-8'
                 );
-
-                $params = array('username' => $username, 'password' => $password, 'ip' => $this->input->ip_address());
                 $this->email->initialize($config);
                 $this->email->set_mailtype("html");
                 $this->email->set_newline("\r\n");
                 //Email content
-                $htmlContent = '<h1 style="color: #5e9ca0;">UPSPHERE SOLUTIONS</h1>';
-                $htmlContent = '<a href="http://ghd.qqt.mybluehost.me//IP/verifyIp/'.base64_encode(json_encode($params)).'"><h1 style="color: #5e9ca0;">UPSPHERE SOLUTIONS</h1></a>';
+                $htmlContent = '<h1 style="color: #5e9ca0;">SATISFOOD IP VERIFICATION</h1>';
+                $htmlContent .= '<label style="color: #5e9ca0;">please copy this link: <a href="http://ghd.qqt.mybluehost.me">'.base64_encode(json_encode($params)).'</a></label>';
                 
                 $this->email->to($result[0]['email']);
-                $this->email->from('no-reply@upspheresolutions.com');
+                $this->email->from('satisfood@upspheresolutions.com');
                 $this->email->subject('NO REPLY!!');
                 $this->email->message($htmlContent);
                 //Send email
@@ -89,7 +90,6 @@ public function sendEmail($username, $password) {
         } else {
                 print_r('Email not found');exit();
         }
-
     }
 
 
